0d302f6f9991db232cdd96ef19c418f814e3a427
Mico Papp <miklos.papp@guidance.hu>
2022-07-16 18:11:40 +0200

    updated installer

--- install.sh
+++ install.sh
@@ -5,18 +5,34 @@ if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X platform
     sudo curl -o /usr/local/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap && sudo chmod +x /usr/local/bin/soap
     INSTALL_PATH=/usr/local/bin/soap
+    if ! command -v xmlstarlet &> /dev/null
+    then
+        brew install xmlstarlet
+    fi
 elif [ "${uname:0:5}" == "Linux" ]; then
     # GNU/Linux platform
     sudo curl -o /usr/local/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap && sudo chmod +x /usr/local/bin/soap
     INSTALL_PATH=/usr/local/bin/soap
+    if ! command -v xmlstarlet &> /dev/null
+    then
+        sudo snap install xmlstarlet
+    fi
 elif [ "${uname:0:10}" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
     curl -o "$HOME/bin/soap" -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
     INSTALL_PATH="$HOME/bin/soap"
+    if ! command -v xmlstarlet &> /dev/null
+    then
+        "xmlstarlet is missing, please install it"
+    fi
 elif [ "${uname:0:10}" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
     curl -o "$HOME/bin/soap" -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
     INSTALL_PATH=$HOME/bin/soap
+    if ! command -v xmlstarlet &> /dev/null
+    then
+        "xmlstarlet is missing, please install it"
+    fi
 fi
 
 if test -f "$INSTALL_PATH"; then
