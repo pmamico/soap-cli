47aed92de3a1590bb401f6d46e395b0483b05a3d
Mico Papp <miklos.papp@guidance.hu>
2022-07-16 16:56:28 +0200

    updated installer

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X platform
     sudo curl -o /usr/local/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap && sudo chmod +x /usr/local/bin/soap
     INSTALL_PATH=/usr/local/bin/soap
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr "$(uname -s)" 1 5)" == "Linux" ]; then
     # GNU/Linux platform
     sudo curl -o /usr/local/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap && sudo chmod +x /usr/local/bin/soap
     INSTALL_PATH=/usr/local/bin/soap
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+elif [ "$(expr substr "$(uname -s)" 1 10)" == "MINGW32_NT" ]; then
     # 32 bits Windows NT platform
-    curl -o $HOME/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
-    INSTALL_PATH=$HOME/bin/soap
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    curl -o "$HOME/bin/soap" -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
+    INSTALL_PATH="$HOME/bin/soap"
+elif [ "$(expr substr "$(uname -s)" 1 10)" == "MINGW64_NT" ]; then
     # 64 bits Windows NT platform
-    curl -o $HOME/bin/soap -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
+    curl -o "$HOME/bin/soap" -s https://raw.githubusercontent.com/pmamico/soap-cli/main/src/soap
     INSTALL_PATH=$HOME/bin/soap
 fi
 
 if test -f "$INSTALL_PATH"; then
-    echo "soap-cli v0.3 successfully installed. ($INSTALL_PATH)"
+    echo "$(soap --version) successfully installed. ($INSTALL_PATH)"
 else 
     echo "Failed to install soap-cli"
 fi
